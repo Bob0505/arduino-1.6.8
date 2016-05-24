0a52fb505a6c58bfdc45f068cc0cb4309240784f
Bob0505 <bob-0505@hotmail.com>
2016-05-25 01:48:08 +0800

    add relay test code.

--- bob/every_oscillate_after/every_oscillate_after.ino
+++ bob/every_oscillate_after/every_oscillate_after.ino
@@ -1,10 +1,11 @@
 #include <Timer.h>
 
+#define RELAY_M 2	/* <bob0525+> add relay test code */
 #define LED_L 13
 
 Timer t;
 
-int ledEvent;
+int RelayEvent, ledEvent;	/* <bob0525+> add relay test code */
 
 void setup()
 {
@@ -17,8 +18,15 @@ void setup()
   Serial.print("0.5 second tick started id=");
   Serial.println(tickEvent);
 
+  pinMode(RELAY_M, OUTPUT);	/* <bob0525+> add relay test code */
   pinMode(LED_L, OUTPUT);
   //oscillate(uint8_t pin, unsigned long period, uint8_t startingValue, -1)   //forever
+  /* <bob0525+> add relay test code */
+  RelayEvent = t.oscillate(RELAY_M, 500, HIGH);
+  Serial.print("Relay event started id=");
+  Serial.println(RelayEvent);
+  /* <bob0525-> add relay test code */
+
   ledEvent = t.oscillate(LED_L, 50, HIGH);
   Serial.print("LED event started id=");
   Serial.println(ledEvent);
